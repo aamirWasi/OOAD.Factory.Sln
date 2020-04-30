b07172d00539afb1fe0f2df3856d358d3648c50c
aamir <wasi3aamir@gmail.com>
2020-04-30 19:39:41 +0600

    separte FighterPlaneFactory from main.cs

--- OOD.FactoryClass11/Program.cs
+++ OOD.FactoryClass11/Program.cs
@@ -2,36 +2,6 @@
 
 namespace OOD.FactoryClass11
 {
-    public class FighterPlaneFactory
-    {
-        FighterPlane fighter = null;
-        public FighterPlane CreatePlane(string selection)
-        {
-            if (selection == "f16")
-            {
-                fighter = new F16Fighter
-                {
-                    MachineGun = 100,
-                    Missile = 35,
-                    Bomb = 57,
-                    Model = "F-16 Fighter Plane",
-                    Speed = 700
-                };
-            }
-            else if (selection == "mig29")
-            {
-                fighter = new Mig29Fighter
-                {
-                    MachineGun = 120,
-                    Missile = 25,
-                    Bomb = 76,
-                    Model = "Mig-29 Alpha Fighter Plane",
-                    Speed = 800
-                };
-            }
-            return fighter;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
